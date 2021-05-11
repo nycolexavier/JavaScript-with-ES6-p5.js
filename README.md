484cb846be153cbfb49e0da333c29230c49f0654
Nycole <nycole.xavierr@gmail.com>
2021-05-11 11:40:20 -0300

    add class in js

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
 # JavaScript with ES6 p5.js
 
 ## Módulo 6
+### Classes 
+```javascript
+let bubble1;
+let bubble2;
+
+
+function setup() {
+  createCanvas(600, 400);
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+}
+
+function draw() {
+  background(0);
+  bubble1.move();
+  bubble1.show();
+  bubble2.move();
+  bubble2.show();
+}
+
+class Bubble {
+  constructor() {
+    this.x = 200;
+    this.y = 150;
+  }
+
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
+
+}
+```
 
 ## Definições importantes:
 
-* Encapsular a ideia de dados e funcionalidades: Class
+* Encapsular a ideia de dados e funcionalidades em um obejto : Class
  
--- modulo6/sketch.js
+++ modulo6/sketch.js
@@ -1,27 +1,37 @@
-var bubble = {
-  x: 300,
-  y: 200,
-}
+let bubble1;
+let bubble2;
 
 
 function setup() {
   createCanvas(600, 400);
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
 }
 
 function draw() {
   background(0);
-  move();
-  display();
+  bubble1.move();
+  bubble1.show();
+  bubble2.move();
+  bubble2.show();
 }
 
-function display () {
-  stroke(255);
-  strokeWeigt(4);
-  noFill();
-  ellipse(bubble.x, bubble.y, 24, 24);
-}
+class Bubble {
+  constructor() {
+    this.x = 200;
+    this.y = 150;
+  }
 
-function move() {
-  bubble.x = bubble.x + random(-5, 5);
-  bubble.y = bubble.y + random(-5, 5);
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
+
 }
